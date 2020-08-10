ed29ef849dc2aeb8fbc3c39561ab01cb463d4781
Gcarter17 <47471988+Gcarter17@users.noreply.github.com>
2020-08-10 04:49:31 -0700

    Updates

---
+++ precache-manifest.5ceb2acd4024e4189b3d38c52fa9cce1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0502fe1ad0988232c73c14e54ff4040",
+    "url": "/blockchains-project/index.html"
+  },
+  {
+    "revision": "dd232262456d1753ce1e",
+    "url": "/blockchains-project/static/css/main.6282f31f.chunk.css"
+  },
+  {
+    "revision": "b0d21b55eb6b03c02040",
+    "url": "/blockchains-project/static/js/2.283dd4f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blockchains-project/static/js/2.283dd4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd232262456d1753ce1e",
+    "url": "/blockchains-project/static/js/main.97741f9b.chunk.js"
+  },
+  {
+    "revision": "1443802dd0f820a44f3c",
+    "url": "/blockchains-project/static/js/runtime-main.a80721bf.js"
+  },
+  {
+    "revision": "5832c4513d214e8f6941c62f418f04b2",
+    "url": "/blockchains-project/static/media/GalaxiePolarisBold.5832c451.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/blockchains-project/static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
